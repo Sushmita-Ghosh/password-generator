ae075228e6359a1de48ad5580588172ee58a15a0
Sushmita Ghosh <pujaghosh251096@gmail.com>
2024-04-11 17:19:05 -0700

    [Generating Password, Copy Password] [Checkbox Fill] [Range]

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import iconCopy from "./assets/images/icon-copy.svg";
 import "./App.css";
+import { useState } from "react";
+import useGeneratedPassword from "./hooks/useGeneratedPassword";
 
 function App() {
-  // checkbox data
-  const checkboxData = [
+  // states - character length & checkbox data
+  const [length, setLength] = useState(0);
+  const [fill, setFill] = useState(0);
+  // to set the text of the copy button
+  const [copy, setCopy] = useState(false);
+  const [checkboxData, setCheckboxData] = useState([
     {
       title: "Include Uppercase Letters",
       state: false,
@@ -20,7 +26,30 @@ function App() {
       title: "Include Symbols",
       state: false,
     },
-  ];
+  ]);
+
+  // custom hook
+  const { password, errorMessage, generatePassword } = useGeneratedPassword();
+
+  // functions
+  // for changing the checkbox state
+  const handleCheckBoxChange = (index) => {
+    const newCheckboxData = [...checkboxData];
+    newCheckboxData[index].state = !newCheckboxData[index].state;
+    setCheckboxData(newCheckboxData);
+  };
+
+  // to copy the generated password
+
+  const handleCopy = () => {
+    navigator.clipboard.writeText(password);
+    setCopy(true);
+
+    // after 1 second, set the copy to false
+    setTimeout(() => {
+      setCopy(false);
+    }, 1000);
+  };
 
   return (
     <div className="h-auto bg-darkest-grey flex justify-center items-center text-center w-full p-10 ">
@@ -34,9 +63,19 @@ function App() {
         {/* Password placeholder & copy */}
 
         <div className="p-4 mb-4 flex justify-between items-center bg-dark-gray">
-          <div className="text-gray text-2xl">edeuigdgd</div>
-          <div className="text-neon-green">
-            <img src={iconCopy} alt="copy icon" />
+          <div
+            className={`${
+              !password ? "text-gray" : "text-almost-white"
+            } text-2xl`}
+          >
+            {password ? password : "jhdkskdgskgs"}
+          </div>
+          <div onClick={handleCopy} className="text-neon-green cursor-pointer">
+            {copy ? (
+              <span className="text-lg">Copied</span>
+            ) : (
+              <img src={iconCopy} alt="copy icon" />
+            )}
           </div>
         </div>
 
@@ -44,38 +83,53 @@ function App() {
           {/* Character length text - value */}
           <div className="flex justify-between items-center">
             <p className="text-almost-white">Character Length</p>
-            <p className="text-neon-green text-3xl">0</p>
+            <p className="text-neon-green text-3xl">{length}</p>
           </div>
 
           {/* Character length slider */}
 
-          <div className="py-4">
+          <div className="py-4 relative">
+            {/* fill bar */}
+            <div
+              className="absolute top-0 bottom-0 my-auto bg-neon-green h-2 w-full"
+              style={{ width: `${fill}%` }}
+            />
+            {/* range bar */}
             <input
               type="range"
               className="appearance-none w-full h-2 cursor-pointer select-none bg-darkest-grey"
-              step="0.1"
-              value="0"
+              step="1"
+              value={length}
               min="0"
               max="20"
-              onChange={() => {}}
+              onChange={(e) => {
+                // percentage of the range
+                setFill((e.target.value / 20) * 100);
+                setLength(e.target.value);
+              }}
             />
           </div>
 
           {/* Checkbox -4 */}
 
           <div className="py-4">
-            {checkboxData.map((data) => (
+            {checkboxData.map((data, index) => (
               <div className="text-md flex items-center py-2" key={data.title}>
                 <input
                   type="checkbox"
-                  className="cursor-pointer"
+                  className="cursor-pointer relative"
                   checked={data.state}
-                  onChange={() => {}}
+                  onChange={() => handleCheckBoxChange(index)}
                 />
                 <p className="pl-4 text-almost-white">{data.title}</p>
               </div>
             ))}
           </div>
+
+          {/* Error Message */}
+          {errorMessage && (
+            <p className="text-red text-sm pb-2">{errorMessage}</p>
+          )}
 
           {/* strength - value */}
 
@@ -94,7 +148,10 @@ function App() {
 
           {/* Password Generator Button */}
           <div className="pt-4">
-            <button className="p-4 border font-bold bg-neon-green hover:fill-neon-green text-darkest-grey hover:border-neon-green hover:bg-darkest-grey transition-transform hover:text-neon-green w-full  uppercase">
+            <button
+              onClick={() => generatePassword(length, checkboxData)}
+              className="p-4 border font-bold bg-neon-green hover:fill-neon-green text-darkest-grey hover:border-neon-green hover:bg-darkest-grey transition-transform hover:text-neon-green w-full  uppercase"
+            >
               Generate
               <svg
                 className="ml-2 inline"
